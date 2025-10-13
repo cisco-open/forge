fbbc632a682db4262763a249a654c2837ee60e7b
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-10-13 15:59:54 +0200

    feat: migrate lambda cur_per_resource to use external packaging

--- modules/integrations/splunk_aws_billing/s3.tf
+++ modules/integrations/splunk_aws_billing/s3.tf
@@ -137,7 +137,7 @@ resource "aws_s3_bucket_notification" "cur_notification" {
   }
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.cur_per_resource.arn
+    lambda_function_arn = module.cur_per_resource.lambda_function_arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "cur-per-resource/aws-billing-report-per-resource/data/"
   }
