000682ff82ba5a647cc4a83c456a0f4c77ea06e6
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-11-28 21:52:20 +0100

    feat: add dashboard for billing

--- modules/integrations/splunk_o11y_conf_shared/dashboard_billing.tf
+++ modules/integrations/splunk_o11y_conf_shared/dashboard_billing.tf
@@ -1,78 +1,192 @@
-# resource "signalfx_dashboard" "billing" {
-#   name            = "Billing"
-#   description     = ""
-#   dashboard_group = signalfx_dashboard_group.forgecicd.id
+resource "signalfx_time_chart" "cost_per_service" {
+  name        = "Cost per service"
+  description = ""
 
-#   time_range = "-31d"
+  program_text = <<-EOF
+A = data('forge.per_service.cost_usd')
+B = A.max(by=['usage_date', 'service', 'forgecicd_tenant','usage_month', 'usage_year'])
+C = B.sum(by=['service', 'forgecicd_tenant','usage_month', 'usage_year'])
 
-#   variable {
-#     property         = "forgecicd_tenant"
-#     alias            = "ForgeCICD Tenant Name"
-#     description      = ""
-#     values           = []
-#     value_required   = false
-#     values_suggested = var.dashboard_variables.runner_k8s.tenant_names
-#   }
+# publish both current and baseline
+C.publish(label='current')
+EOF
 
-#   dynamic "variable" {
-#     for_each = var.dashboard_variables.billing.dynamic_variables
-#     content {
-#       property         = each.value.property
-#       alias            = each.value.alias
-#       description      = each.value.description
-#       values           = each.value.values
-#       value_required   = each.value.value_required
-#       values_suggested = each.value.values_suggested
-#     }
-#   }
+  plot_type = "AreaChart"
+}
 
+resource "signalfx_time_chart" "net_cost_per_service" {
+  name        = "Net Cost per service"
+  description = ""
 
-#   # Chart layouts
-#   chart {
-#     chart_id = signalfx_time_chart.cost_per_service.id
-#     width    = 6
-#     height   = 1
-#     row      = 0
-#     column   = 0
-#   }
+  program_text = <<-EOF
+A = data('forge.per_service.net_cost_usd')
+B = A.max(by=['usage_date', 'service', 'forgecicd_tenant','usage_month', 'usage_year'])
+C = B.sum(by=['service', 'forgecicd_tenant','usage_month', 'usage_year'])  # removes usage_date from label
 
-#   chart {
-#     chart_id = signalfx_time_chart.net_cost_per_service.id
-#     width    = 6
-#     height   = 1
-#     row      = 0
-#     column   = 6
-#   }
+# publish both current and baseline
+C.publish(label='current')
+EOF
 
-#   chart {
-#     chart_id = signalfx_time_chart.net_cost_per_tenant.id
-#     width    = 6
-#     height   = 1
-#     row      = 1
-#     column   = 6
-#   }
+  plot_type = "AreaChart"
+}
 
-#   chart {
-#     chart_id = signalfx_time_chart.cost_per_tenant.id
-#     width    = 6
-#     height   = 1
-#     row      = 1
-#     column   = 0
-#   }
+resource "signalfx_time_chart" "net_cost_per_tenant" {
+  name        = "Net Cost per tenant"
+  description = ""
 
-#   chart {
-#     chart_id = signalfx_time_chart.total_net_cost.id
-#     width    = 6
-#     height   = 1
-#     row      = 2
-#     column   = 6
-#   }
+  program_text = <<-EOF
+A = data('forge.per_service.net_cost_usd')
+B = A.max(by=['usage_date', 'service', 'forgecicd_tenant','usage_month', 'usage_year'])
+C = B.sum(by=['forgecicd_tenant','usage_month', 'usage_year'])
+D = C.timeshift('29d')
 
-#   chart {
-#     chart_id = signalfx_time_chart.total_cost.id
-#     width    = 6
-#     height   = 1
-#     row      = 2
-#     column   = 0
-#   }
-# }
+# publish both current and baseline
+C.publish(label='current')
+#D.publish(label='baseline')
+EOF
+
+  plot_type = "AreaChart"
+}
+
+resource "signalfx_time_chart" "cost_per_tenant" {
+  name        = "Cost per tenant"
+  description = ""
+
+  program_text = <<-EOF
+A = data('forge.per_service.cost_usd')
+B = A.max(by=['usage_date', 'service', 'forgecicd_tenant','usage_month', 'usage_year'])
+C = B.sum(by=['forgecicd_tenant','usage_month', 'usage_year'])
+D = C.timeshift('29d')
+
+# publish both current and baseline
+C.publish(label='current')
+#D.publish(label='baseline')
+EOF
+
+  plot_type = "AreaChart"
+}
+
+resource "signalfx_time_chart" "total_cost" {
+  name        = "Total Cost"
+  description = ""
+
+  program_text = <<-EOF
+A = data('forge.per_service.cost_usd')
+
+# Take max per day/service/tenant, carrying forward last value if missing
+B = A.max(by=['usage_date', 'service', 'forgecicd_tenant','usage_month', 'usage_year'])
+
+# Sum by month, still carrying forward where needed
+C = B.sum(by=['usage_month', 'usage_year'])
+
+# Shift by 29 days to get a baseline comparison
+D = C.timeshift('29d')
+
+# Publish both
+C.publish(label='current')
+D.publish(label='baseline')
+EOF
+
+  plot_type = "AreaChart"
+}
+
+resource "signalfx_time_chart" "total_net_cost" {
+  name        = "Total Net Cost"
+  description = ""
+
+  program_text = <<-EOF
+A = data('forge.per_service.net_cost_usd')
+
+# Take max per day/service/tenant, carrying forward last value if missing
+B = A.max(by=['usage_date', 'service', 'forgecicd_tenant','usage_month', 'usage_year'])
+
+# Sum by month, still carrying forward where needed
+C = B.sum(by=['usage_month', 'usage_year'])
+
+# Shift by 29 days to get a baseline comparison
+D = C.timeshift('29d')
+
+# Publish both
+C.publish(label='current')
+# D.publish(label='baseline')
+EOF
+
+  plot_type = "AreaChart"
+}
+
+resource "signalfx_dashboard" "billing" {
+  name            = "Billing"
+  description     = ""
+  dashboard_group = signalfx_dashboard_group.forgecicd.id
+
+  time_range = "-31d"
+
+  variable {
+    property         = "forgecicd_tenant"
+    alias            = "ForgeCICD Tenant Name"
+    description      = ""
+    values           = []
+    value_required   = false
+    values_suggested = var.dashboard_variables.runner_k8s.tenant_names
+  }
+
+  dynamic "variable" {
+    for_each = var.dashboard_variables.billing.dynamic_variables
+    content {
+      property         = each.value.property
+      alias            = each.value.alias
+      description      = each.value.description
+      values           = each.value.values
+      value_required   = each.value.value_required
+      values_suggested = each.value.values_suggested
+    }
+  }
+
+  chart {
+    chart_id = signalfx_time_chart.cost_per_service.id
+    row      = 0
+    column   = 0
+    width    = 6
+    height   = 1
+  }
+
+  chart {
+    chart_id = signalfx_time_chart.net_cost_per_service.id
+    row      = 0
+    column   = 6
+    width    = 6
+    height   = 1
+  }
+
+  chart {
+    chart_id = signalfx_time_chart.cost_per_tenant.id
+    row      = 1
+    column   = 0
+    width    = 6
+    height   = 1
+  }
+
+  chart {
+    chart_id = signalfx_time_chart.net_cost_per_tenant.id
+    row      = 1
+    column   = 6
+    width    = 6
+    height   = 1
+  }
+
+  chart {
+    chart_id = signalfx_time_chart.total_cost.id
+    row      = 2
+    column   = 0
+    width    = 6
+    height   = 1
+  }
+
+  chart {
+    chart_id = signalfx_time_chart.total_net_cost.id
+    row      = 2
+    column   = 6
+    width    = 6
+    height   = 1
+  }
+}
