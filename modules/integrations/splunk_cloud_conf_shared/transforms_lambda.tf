d43ed3565914ce90b959bbef20023a8ccf1e94b5
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-12-01 20:35:24 +0100

    feat: create field extraction forgecicd_trust_validation

--- modules/integrations/splunk_cloud_conf_shared/transforms_lambda.tf
+++ modules/integrations/splunk_cloud_conf_shared/transforms_lambda.tf
@@ -29,3 +29,37 @@ resource "splunk_configs_conf" "forgecicd_extra_lambda_tenant_fields" {
     ]
   }
 }
+
+
+resource "splunk_configs_conf" "forgecicd_trust_validation" {
+  name = "transforms/forgecicd_trust_validation"
+
+  variables = {
+    REGEX      = "Validation complete:\\s*(\\[[^\\r\\n]+])"
+    FORMAT     = "forgecicd_trust_validation::$1"
+    SOURCE_KEY = "_raw"
+    CLEAN_KEYS = "0"
+  }
+
+  acl {
+    app     = var.splunk_conf.acl.app
+    owner   = var.splunk_conf.acl.owner
+    sharing = var.splunk_conf.acl.sharing
+    read    = var.splunk_conf.acl.read
+    write   = var.splunk_conf.acl.write
+  }
+  lifecycle {
+    ignore_changes = [
+      variables["CAN_OPTIMIZE"],
+      variables["DEFAULT_VALUE"],
+      variables["DEPTH_LIMIT"],
+      variables["DEST_KEY"],
+      variables["KEEP_EMPTY_VALS"],
+      variables["LOOKAHEAD"],
+      variables["MATCH_LIMIT"],
+      variables["MV_ADD"],
+      variables["WRITE_META"],
+      variables["disabled"]
+    ]
+  }
+}
