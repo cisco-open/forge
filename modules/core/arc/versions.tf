975220e1ab1e876bf54fca7b1841f79a77f243dd
edersonbrilhante <contato@edersonbrilhante.com.br>
2025-12-09 00:52:58 +0100

    docs: update terraform docs

--- modules/core/arc/versions.tf
+++ modules/core/arc/versions.tf
@@ -14,7 +14,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.36.0"
+      version = ">= 3.0"
     }
     null = {
       source  = "hashicorp/null"
