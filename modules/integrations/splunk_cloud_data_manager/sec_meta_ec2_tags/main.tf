770222efcc832c391a290e233316e826f187cf38
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-02-05 20:01:19 +0100

    chore: update aws lambda to 8.2.1

--- modules/integrations/splunk_cloud_data_manager/sec_meta_ec2_tags/main.tf
+++ modules/integrations/splunk_cloud_data_manager/sec_meta_ec2_tags/main.tf
@@ -1,6 +1,6 @@
 module "splunk_dm_metadata_ec2inst_pattern_tags_lambda" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "8.1.2"
+  version = "8.2.1"
 
   role_name = "SplunkDMMetadataEC2InstPatternTags-${var.region}"
 
