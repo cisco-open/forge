d5cf50b8483d34f9475c1d04d5a5ad704c261df0
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-06-13 02:55:39 +0200

    fix(splunk): filter disk summary by tenant tag

--- modules/integrations/splunk_o11y_conf_shared/dashboards/runner_ec2/main.tf
+++ modules/integrations/splunk_o11y_conf_shared/dashboards/runner_ec2/main.tf
@@ -1290,8 +1290,8 @@ resource "signalfx_list_chart" "chart_disk_summary_utilization" {
   description = "Percent of disk space utilized on all volumes on active hosts with agent installed. Instance id | Host"
 
   program_text = <<-EOF
-A = data('system.filesystem.usage', filter=filter('cloud.platform', 'aws_ec2', 'aws_eks') and filter('state', 'used')).publish(label='A', enable=False)
-B = data('system.filesystem.usage', filter=filter('cloud.platform', 'aws_ec2', 'aws_eks') and filter('state', 'free')).publish(label='B', enable=False)
+A = data('system.filesystem.usage', filter=filter('cloud.platform', 'aws_ec2', 'aws_eks') and filter('aws_tag_TenantName', '*') and filter('state', 'used')).publish(label='A', enable=False)
+B = data('system.filesystem.usage', filter=filter('cloud.platform', 'aws_ec2', 'aws_eks') and filter('aws_tag_TenantName', '*') and filter('state', 'free')).publish(label='B', enable=False)
 C = ((A/(A+B))*100).mean(by=['host.name', 'AWSUniqueId', 'aws_tag_TenantName']).publish(label='C')
 EOF
 
