ba5a0829237f0d52af63e39f3a1444953644ed04
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-07-02 20:19:04 +0200

    chore(deps): bump OpenCost Helm chart

--- modules/integrations/splunk_opencost_eks/main.tf
+++ modules/integrations/splunk_opencost_eks/main.tf
@@ -44,7 +44,7 @@ resource "helm_release" "opencost" {
   name             = "opencost"
   repository       = "https://opencost.github.io/opencost-helm-chart"
   chart            = "opencost"
-  version          = "2.5.20"
+  version          = "2.5.25"
   namespace        = "opencost"
   create_namespace = true
 
