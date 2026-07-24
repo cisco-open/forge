d1d29a168e2e9bd8e0a65689da465b0c20456c70
Ederson Brilhante <contato@edersonbrilhante.com.br>
2026-07-25 00:58:15 +0200

    feat(observability): add dependency probes dashboard (#516)

---
+++ modules/integrations/splunk_o11y_conf_shared/dashboards/dependency_probes/variables.tf
@@ -0,0 +1,23 @@
+variable "dashboard_group" {
+  description = "Splunk Observability dashboard group ID."
+  type        = string
+}
+
+variable "dynamic_variables" {
+  description = "Additional dynamic variable definitions for the dashboard."
+  type = list(object({
+    property               = string
+    alias                  = string
+    description            = string
+    values                 = list(string)
+    value_required         = bool
+    values_suggested       = list(string)
+    restricted_suggestions = bool
+  }))
+  default = []
+}
+
+variable "tenant_names" {
+  description = "Forge tenants available in the dashboard selector."
+  type        = list(string)
+}
