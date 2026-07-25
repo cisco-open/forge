4cfa2c8922410a67464ff4c229acee1d7ddbbe6c
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-07-25 03:11:21 +0200

    fix(observability): extract shared Lambda fields

--- modules/integrations/splunk_cloud_conf_shared/transforms_lambda.tf
+++ modules/integrations/splunk_cloud_conf_shared/transforms_lambda.tf
@@ -1,3 +1,35 @@
+resource "splunk_configs_conf" "forgecicd_shared_lambda_fields" {
+  name = "transforms/forgecicd_shared_lambda_fields"
+
+  variables = {
+    "REGEX"      = "(?<aws_region>[^:]+):\\/aws\\/lambda\\/(?<forgecicd_log_type>splunk-dependency-monitor|splunk-s3-runner-logs-lambda|forge-aws-billing-per-service|forge-aws-billing-per-resource-process|forge-aws-billing-per-resource|webex-webhook-relay-destination-receiver|SplunkDMMetadataEC2InstPatternTags|SplunkDMMetadataEC2Inst)(?:-[a-z]{2}(?:-[a-z]+)+-[0-9]+)?:"
+    "FORMAT"     = "aws_region::$1 forgecicd_log_type::$2"
+    "SOURCE_KEY" = "source"
+    "CLEAN_KEYS" = "0"
+  }
+  acl {
+    app     = var.splunk_conf.acl.app
+    owner   = var.splunk_conf.acl.owner
+    sharing = var.splunk_conf.acl.sharing
+    read    = var.splunk_conf.acl.read
+    write   = var.splunk_conf.acl.write
+  }
+  lifecycle {
+    ignore_changes = [
+      variables["CAN_OPTIMIZE"],
+      variables["DEFAULT_VALUE"],
+      variables["DEPTH_LIMIT"],
+      variables["DEST_KEY"],
+      variables["KEEP_EMPTY_VALS"],
+      variables["LOOKAHEAD"],
+      variables["MATCH_LIMIT"],
+      variables["MV_ADD"],
+      variables["WRITE_META"],
+      variables["disabled"]
+    ]
+  }
+}
+
 resource "splunk_configs_conf" "forgecicd_extra_lambda_tenant_fields" {
   name = "transforms/forgecicd_extra_lambda_tenant_fields"
 
