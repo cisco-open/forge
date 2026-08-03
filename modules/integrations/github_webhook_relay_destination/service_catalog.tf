140c70cc33c568d6185d8b2c3804b392c2eda5ef
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-08-03 11:24:54 +0200

    fix: remove service application from github_webhook_relay_destination

--- modules/integrations/github_webhook_relay_destination/service_catalog.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_servicecatalogappregistry_application" "this" {
-  name = "integrations_github_webhook_relay_destination_${var.aws_region}"
-  tags = merge(var.default_tags, var.tags)
-}
