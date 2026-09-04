7f04dd502590ce816861455e8233cebac4890946
edersonbrilhante <contato@edersonbrilhante.com.br>
2026-09-04 15:58:04 +0200

    fix(runners): use multi-runner v2 routing ref

--- modules/platform/ec2_deployment/main.tf
+++ modules/platform/ec2_deployment/main.tf
@@ -126,7 +126,7 @@ resource "aws_iam_policy" "runner_hooks_ssm_read" {
 
 module "runners" {
   #checkov:skip=CKV_TF_1:Module source uses Renovate-managed version tags; commit SHA pinning is an accepted policy tradeoff.
-  source = "git::https://github.com/github-aws-runners/terraform-aws-github-runner.git//modules/multi-runner?ref=v7.11.0"
+  source = "git::https://github.com/github-aws-runners/terraform-aws-github-runner.git//modules/multi-runner?ref=fix-multi-runner-v2-routing"
 
   aws_region = var.aws_region
 
